b146021055e18b0bae2d12f5eced601d1836e7b6
Jack Zhang <jack.zhang@aspiraconnect.com>
2020-11-25 20:30:19 +0800

    fix #12 add staticfiles

--- DevOpsTool/settings.py
+++ DevOpsTool/settings.py
@@ -143,6 +143,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
